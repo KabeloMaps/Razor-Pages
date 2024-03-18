e19833ba08434e264ce14423f0d69852dfee3ffb
Natasha Dzinotyiweyi <DZNNAT001@myuct.ac.za>
2024-03-18 12:38:38 +0200

    changed title to movieTitle

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -5,7 +5,7 @@ namespace RazorPagesMovie.Models;
 public class Movie
 {
     public int Id { get; set; }  //this is the attributes for the website
-    public string? Title { get; set; }
+    public string? movieTitle { get; set; }    //changed Title to movieTitle
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
